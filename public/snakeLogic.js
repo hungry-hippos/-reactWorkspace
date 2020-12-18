0e0f8048a3fb6b3c9b9229c44a06bcc2dc24aa89
Hungry Hippo <aduca96@gmail.com>
2020-12-17 18:14:17 -0800

    snake updates

--- public/snakeLogic.js
+++ public/snakeLogic.js
@@ -1,15 +1,18 @@
 var mainGrid=document.getElementById('mainGrid');
-for (var i=0;i<225;i++){
+for (var i=0;i<525;i++){
 	var square=document.createElement('div');
 	square.classList.add('snakeSquare');
+	square.setAttribute('key', i);
 	mainGrid.appendChild(square);
 };
 
 var squares=document.getElementsByClassName('snakeSquare');
-
+var bodyLocation=[];
+var headLocation=createSnakeHead();
+var ratLocation=ratCreation();
 
 function createSnakeHead(){
-	var spawnSq=Math.floor(Math.random()*223);
+	var spawnSq=Math.floor(Math.random()*524);
 	squares[spawnSq].setAttribute('id','snakeHead');
 	return spawnSq;
 }
@@ -17,7 +20,7 @@ function createSnakeHead(){
 function ratCreation(){
   //random num that is not the snakehead
   while (true){
-    var ratSpawnLocation=Math.floor(Math.random()*223);
+    var ratSpawnLocation=Math.floor(Math.random()*524);
     if (ratSpawnLocation===headLocation){
       continue;
     }
@@ -36,15 +39,13 @@ function ratCreation(){
 
   }	
 }
-var headLocation=createSnakeHead();
-var bodyLocation=[];
-var ratLocation=ratCreation();
+
 
 
 function headMovement(){
 	if (movementDirection=='left'){
 		snakeSuicide(headLocation-1);
-		if (headLocation%15==0)
+		if (headLocation%35==0)
 			snakeDead();
 		else if (headLocation-1==ratLocation)
 			snakeEats(headLocation-1);
@@ -52,17 +53,17 @@ function headMovement(){
 			snakeMoves(headLocation-1);
 	}
 	else if (movementDirection=='up'){
-		snakeSuicide(headLocation-15);
-		if (0<=headLocation && headLocation<=14)
+		snakeSuicide(headLocation-35);
+		if (0<=headLocation && headLocation<=34)
 			snakeDead();
-		else if (headLocation-15==ratLocation)
-			snakeEats(headLocation-15);
+		else if (headLocation-35==ratLocation)
+			snakeEats(headLocation-35);
 		else
-			snakeMoves(headLocation-15);
+			snakeMoves(headLocation-35);
 	}
 	else if (movementDirection=='right'){
 		snakeSuicide(headLocation+1);
-		if ((headLocation+1)%15==0)
+		if ((headLocation+1)%35==0)
 			snakeDead();
 		else if (headLocation+1==ratLocation)
 			snakeEats(headLocation+1);
@@ -70,13 +71,13 @@ function headMovement(){
 			snakeMoves(headLocation+1);
 	}
 	else if (movementDirection=='down'){
-		snakeSuicide(headLocation+15);
-		if (210<=headLocation && headLocation<=223)
+		snakeSuicide(headLocation+35);
+		if (490<=headLocation && headLocation<=524)
 			snakeDead();
-		else if (headLocation+15==ratLocation)
-			snakeEats(headLocation+15);
+		else if (headLocation+35==ratLocation)
+			snakeEats(headLocation+35);
 		else
-			snakeMoves(headLocation+15);
+			snakeMoves(headLocation+35);
 	}
 }
 
@@ -88,8 +89,22 @@ function snakeSuicide(nextHead){
 }
 function snakeDead(){
 	clearInterval(currentIntervalCode);
-	document.removeEventListener('keydown',changeDirection);
-	alert('snek is ded');
+	for (var i=0;i<bodyLocation.length;i++){
+		squares[bodyLocation[i]].classList.remove('snakeBody');
+	}
+	squares[headLocation].removeAttribute('id');
+	squares[ratLocation].removeAttribute('id');
+
+	headLocation=createSnakeHead();
+	bodyLocation=[];
+	ratLocation=ratCreation();
+	
+	if (document.getElementById('gameConfirmText').textContent!='Submit when ready.'){
+		document.getElementById('appleCounter').textContent="10";
+	}
+
+	time=150;
+	
 }
 function snakeEats(nextHead){
 	squares[headLocation].removeAttribute('id');
@@ -98,7 +113,21 @@ function snakeEats(nextHead){
 	squares[nextHead].setAttribute('id','snakeHead');
 	headLocation=nextHead;
 	ratLocation=ratCreation();
-	console.log('eats');
+	
+	if (document.getElementById('gameConfirmText').textContent!='Submit when ready.'){
+		const counter=document.getElementById('appleCounter');
+		var num=parseInt(counter.textContent,10);
+		num--;
+		if (num===0){
+			document.getElementById('gameConfirmText').textContent='Submit when ready.'
+			document.getElementById('gameConfirmBtn').disabled=false;
+			return;
+		}
+		counter.textContent=num;
+	}
+	if (time>=80){
+		time=time-5;
+	}
 }
 function snakeMoves(nextHead){
 	squares[headLocation].removeAttribute('id');
@@ -113,25 +142,26 @@ function snakeMoves(nextHead){
 document.addEventListener('keydown',changeDirection);
 var currentIntervalCode=0;
 var movementDirection="";
+var time=150;
 function changeDirection(event){
 	clearInterval(currentIntervalCode);
-	if (event.keyCode==37){
+	if (event.keyCode==37 && movementDirection!='right'){
 		//move left
 		movementDirection='left';
 	}
-	if (event.keyCode==38){
+	if (event.keyCode==38 && movementDirection!='down'){
 		//move up
 		movementDirection='up';
 	}
-	if (event.keyCode==39){
+	if (event.keyCode==39 && movementDirection!='left'){
 		//move right
 		movementDirection='right';
 	}
-	if (event.keyCode==40){
+	if (event.keyCode==40 && movementDirection!='up'){
 		//move down
 		movementDirection='down';
 	}
-	currentIntervalCode=setInterval(headMovement,150);
+	currentIntervalCode=setInterval(headMovement,time);
 }
 
 
--- src/frustratingForms/MinigamesForm.js
+++ src/frustratingForms/MinigamesForm.js
@@ -1,30 +1,47 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 
 import './MinigamesForm.css'
 
 
 const MinigamesForm=()=>{
 
+    //appends a script tag, drawing it from public folder
     const appendScript=()=>{
         const s=document.createElement('script');
         s.src='/snakeLogic.js';
         document.body.appendChild(s);
     }
+    const loadBtnHoverListener=()=>{
+        console.log("CHANGE");
+        document.getElementById('submitGameBtn').addEventListener('mouseenter',(event)=>{
+            document.getElementById('gameConfirmDiv').style.opacity='1';
+            event.target.style.opacity='0';
+        })
+    }
+
+    useEffect(()=>{
+        appendScript();
+    },[])
 
 
-    return <div id='minigameMain'>
+    return <>
+        <div id='minigameMain'>
                 <div className='minigameFormSection'>
-                    <label htmlFor='firstName'>First Name: </label>
-                    <input type='text' id='firstName'></input>
+                    <label htmlFor='firstName' className='nameLabel'>First Name: </label>
+                    <input type='text' id='firstName' className='nameInput'></input>
                 </div>
                 <div className='minigameFormSection'>
-                    <label htmlFor='lastName'>Last Name:</label>
-                    <input type='text' id='lastName'></input>
+                    <label htmlFor='lastName' className='nameLabel'>Last Name:</label>
+                    <input type='text' id='lastName' className='nameInput' onChange={loadBtnHoverListener}></input>
                 </div>
-                <button type='button' id='submitNameBtn' onClick={appendScript}>SUBMIT</button> 
-
-                <div id="mainGrid"></div>
-            </div>
+                <button type='button' id='submitGameBtn'>SUBMIT</button> 
+        </div>
+        <div id='gameConfirmDiv'>
+            <div id='gameConfirmText'>Eat <div id='appleCounter' style={{fontWeight:'bold', display:'inline'}}>10</div> apples to submit.</div>
+            <div id="mainGrid"></div>
+            <button id='gameConfirmBtn' disabled>SUBMIT</button>
+        </div>
+        </>
 }
 
 export default MinigamesForm
