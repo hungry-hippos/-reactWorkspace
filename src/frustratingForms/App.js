e8b3050960400a5f624d24523d431ce28e2fc1b8
Hungry Hippo <aduca96@gmail.com>
2020-12-23 12:24:07 -0800

    some styling updates

--- src/frustratingForms/App.js
+++ src/frustratingForms/App.js
@@ -3,19 +3,21 @@ import Robot from './Robot.js'
 import DOB from './DOB'
 import Minigame from './MinigamesForm'
 import MapContainer from './MapContainer'
+import ContactInfo from './ContactInfo'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 
 
 const SideBar=(props)=>{
-  const {setShowRobot,setShowDOB,setShowMinigame,setShowMap}=props.setters;
+  const {setShowRobot,setShowDOB,setShowMinigame,setShowMap,setShowContactInfo}=props.setters;
 
   const show=(component)=>{
     setShowRobot(false);
     setShowDOB(false);
     setShowMinigame(false);
     setShowMap(false);
+    setShowContactInfo(false);
     switch(component){
       case "robot":
         setShowRobot(true);
@@ -29,6 +31,9 @@ const SideBar=(props)=>{
       case "map":
         setShowMap(true);
         break;
+      case "contact":
+        setShowContactInfo(true);
+        break;
       default:
         break;
     }
@@ -38,6 +43,7 @@ const SideBar=(props)=>{
     <button onClick={()=>{show("dob")}} className="mainPageBtn">DOB</button>
     <button onClick={()=>{show('minigame')}} className='mainPageBtn'>Minigame</button>
     <button onClick={()=>{show('map')}} className='mainPageBtn'>Map</button>
+    <button onClick={()=>{show('contact')}} className='mainPageBtn'>Contact</button>
 
   </div>
 
@@ -47,16 +53,18 @@ function App() {
   const [showRobot,setShowRobot]=useState(false);
   const [showDOB,setShowDOB]=useState(false);
   const [showMinigame,setShowMinigame]=useState(false);
-  const [showMap,setShowMap]=useState(true);
+  const [showMap,setShowMap]=useState(false);
+  const [showContactInfo,setShowContactInfo]=useState(true);
 
-  const setters={setShowRobot,setShowDOB,setShowMinigame,setShowMap};
+  const setters={setShowRobot,setShowDOB,setShowMinigame,setShowMap,setShowContactInfo};
   
   return <React.Fragment>
         <SideBar setters={setters} />
-        { showRobot && <Robot />}
+        {showRobot && <Robot />}
         {showDOB && <DOB />}
         {showMinigame && <Minigame />}
         {showMap && <MapContainer />}
+        {showContactInfo && <ContactInfo />}
   </React.Fragment>
   
   
---
+++ src/frustratingForms/ContactInfo.js
@@ -0,0 +1,122 @@
+import React,{useState} from 'react'
+import Button from 'react-bootstrap/Button'
+import "./ContactInfo.css"
+
+
+const ContactInfo=()=>{
+    var [address,setAddress]=useState('');
+    var [domain,setDomain]=useState('');
+    var [phone,setPhone]=useState('');
+    var [isUppercase,setIsUppercase]=useState(true);
+    
+
+    const changeAddress=(e)=>{
+        const inputVal=e.target.value;
+        var newVal="";
+        //if user used backspace, update by erasing left-most char. Else, add newest char as left-most char
+        if (inputVal.length<address.length){
+            for (var i=1;i<address.length;i++){
+                newVal+=address[i];
+            }
+        }else{
+            const newestChar=inputVal[inputVal.length-1];
+            newVal=newestChar+address;
+        }
+        setAddress(newVal);
+    }
+
+    const changeDomain=(e)=>{
+        var inputVal=e.target.value;
+
+        //case where user deletes input value
+        if (inputVal.length<domain.length){
+            setDomain(inputVal);
+            return;
+        }
+
+        var newDomain="";
+        if (isUppercase){
+            //find the changed character on the string. Compare inputVal with domain.
+            for (var i=0;i<inputVal.length;i++){
+                if (inputVal[i]===domain[i]){
+                    newDomain+=inputVal[i];
+                }else{
+                    var diffChar=inputVal[i];
+                    newDomain=newDomain+diffChar.toUpperCase();
+
+                    for (var t=i;t<domain.length;t++){
+                        newDomain+=domain[t];
+                    }
+                    break;
+                }
+            }
+            setIsUppercase(false);
+        }else{
+            newDomain=inputVal;
+            setIsUppercase(true);
+        }
+        setDomain(newDomain);
+    }
+
+    const generatePhone=()=>{
+
+        var number="";
+        for (var i=0;i<10;i++){
+            var digit=Math.floor((Math.random()*10));
+            number+=digit;
+        }
+        const areaCode=number[0]+number[1]+number[2];
+        const firstDigitSet=number[3]+number[4]+number[5];
+        const secondDigitSet=number[6]+number[7]+number[8]+number[9];
+        number="("+areaCode+") "+firstDigitSet+"-"+secondDigitSet;
+        
+        setPhone(number);
+
+        document.getElementById('contactThisIsIt').style.opacity="1";
+        document.getElementById('contactGenerate').textContent='RETRY';
+    }
+
+    const unveilBottom=()=>{
+        document.getElementById('contactInfoMain').style.height="450px";
+        setTimeout(()=>{document.getElementById('confirmationBottom').style.opacity="1";
+        document.getElementById('contactThisIsIt').style.display='none';
+            },1000)
+        document.getElementById('contactThisIsIt').style.opacity='0';
+    }
+
+
+    return <div id='contactInfoMain'>
+        <div id="contactTitle">Contact Info</div>
+        <hr id='contactHr'/>
+                
+        <div id='contactEmailForm'>
+            <label htmlFor='address'>Email: </label>
+            <input type='text' id='address' value={address} onChange={(e)=>changeAddress(e)}></input>
+            <label htmlFor='domain'>@</label>
+            <input type='text' id='domain' value={domain} onChange={(e)=>changeDomain(e)}></input>
+        </div>
+        <div id='contactPhoneForm'>
+            <label htmlFor='phone'>Phone: </label>
+            <input type='text' id='phone' value={phone} disabled></input>
+            <Button variant='outline-dark' id='contactGenerate' onClick={generatePhone}>GENERATE</Button>
+        </div>
+        
+        <Button variant='outline-primary' id='contactThisIsIt' onClick={unveilBottom}>This is my number</Button>
+
+        <div id='confirmationBottom'>
+            <div id='SMSverification'>
+                <div id='SMStext'>Please verify your phone number. Enter the 4 digit SMS verification code you received.</div>
+                <div id='disclaimer'>* Standard rates and fees may apply</div>
+            </div>
+            <input type='text' id='SMScode'></input>
+
+            <div id='confirmationBtns'>
+                <Button id='confirmationSubmit'>SUBMIT</Button>
+                <Button variant='outline-dark' id='confirmationSkip'>SKIP THIS STEP</Button>
+            </div>
+        </div>
+
+    </div>
+}
+
+export default ContactInfo
